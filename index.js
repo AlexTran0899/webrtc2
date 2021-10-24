e8d84c81fcce7e3c58641bdcfce21b06e33d027b
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-10-23 22:45:46 -0500

    changed react app directoy

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const io = require("socket.io")(server, {
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
   
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'))
 })
-
+//
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
 
